b32b45ec67822380f219eaf20607cb894e6d7e5d
Ashwini-D2008 <ashwinidete99@gmail.com>
2026-07-16 15:59:57 +0530

    Update fun.py

--- fun.py
+++ fun.py
@@ -5,7 +5,7 @@ import pickle
 
 st.title("Movie Recommender System")
 df = pd.read_csv('cleaned_data.csv')
-pickle.load(open('similarity.pkl', 'wb'))
+pickle.load(open('similarity.pkl', 'rb'))
 
 movies = df['title'].tolist()
 
